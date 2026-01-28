071dbac9ec99453244147fcbdde273f3080dd269
sptrading <sptrading300123@gmail.com>
2026-01-28 15:47:56 +0530

    Update scanner.py

--- routes/scanner.py
+++ routes/scanner.py
@@ -1,19 +1,8 @@
 from fastapi import APIRouter
-from services.data_collector import collect_data
-from services.market_data import run_scanner
+from services.market_data import get_all_stocks
 
 router = APIRouter()
 
-
 @router.get("/scanner")
 def scanner():
-    # First collect fresh data
-    collect_data()
-
-    # Then run maths on stored data
-    breakout, boost = run_scanner()
-
-    return {
-        "breakout": breakout[:10],
-        "intraday_boost": boost[:10],
-    }
+    return get_all_stocks()
