4dbb1123b66817434753a92e46802a008d3a490b
sptrading <sptrading300123@gmail.com>
2026-01-28 15:14:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,28 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from routes.scanner import router as scanner_router
 
+from services.data_collector import collect_data
+import threading
+import time
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(scanner_router)
 
 
-@app.get("/")
-def dashboard(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-from services.data_collector import collect_data
-import threading
-import time
-
-
 def background_collector():
     while True:
         collect_data()
-        time.sleep(60)  # प्रत्येक 1 मिनिटाला data store
+        time.sleep(60)
 
 
 @app.on_event("startup")
 def start_collector():
     thread = threading.Thread(target=background_collector, daemon=True)
     thread.start()
+
+
+@app.get("/")
+def dashboard(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
