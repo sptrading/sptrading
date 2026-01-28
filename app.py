903b6b04f08a53895b5c5d517fcfb3c820df595b
sptrading <sptrading300123@gmail.com>
2026-01-28 14:59:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,18 @@ app.include_router(scanner_router)
 @app.get("/")
 def dashboard(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+from services.data_collector import collect_data
+import threading
+import time
+
+
+def background_collector():
+    while True:
+        collect_data()
+        time.sleep(60)  # प्रत्येक 1 मिनिटाला data store
+
+
+@app.on_event("startup")
+def start_collector():
+    thread = threading.Thread(target=background_collector, daemon=True)
+    thread.start()
