dc0f300602cf3cd0fdc2b2f6ef749d5270b76cef
sptrading <sptrading300123@gmail.com>
2026-01-28 11:58:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles
-
 from routes.scanner import router as scanner_router
 
 app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 app.include_router(scanner_router)
